6c44eb166ea6b7aa2eccfae2b6a85a52de04a18b
Danil IenbaIev <ennnbaev@icloud.com>
2021-08-02 00:53:29 +0300

    start

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        {
+            //изминения в  feature-1
+
+        }
+        {
+            //изминения в  feature-2
+
+        }
+        {
+            //изминения в  feature-2-bugfix
+        }
+    }
+}
