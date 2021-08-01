10fd8c12a403a929d0c8fcfc6f174e58021b8d5f
Danil IenbaIev <ennnbaev@icloud.com>
2021-08-02 01:01:33 +0300

    error in word

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,7 +11,7 @@ public class Main {
             //изменения в  feature-2
         }
         {
-            //изминения в  feature-2-bugfix
+            //изменения в  feature-2-bugfix
         }
     }
 
